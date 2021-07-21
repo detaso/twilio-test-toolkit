5b5a167610e57ca3cdd7aa52c545d39ccf7cf925
Aaron Wortham <wortham.aaron@gmail.com>
2021-07-21 14:14:34 -0600

    Remove a couple of useless option setters

--- lib/twilio-test-toolkit/call_in_progress.rb
+++ lib/twilio-test-toolkit/call_in_progress.rb
@@ -10,27 +10,14 @@ module TwilioTestToolkit
     attr_reader :sid, :initial_path, :from_number, :to_number, :is_machine, :http_method
 
     def initialize(initial_path, from_number, to_number, options = {})
+      options[:method] ||= :post # sets option for the `request_for_twiml!`
       # Save our variables for later
       @initial_path = initial_path
       @from_number = from_number
       @to_number = to_number
       @is_machine = options[:is_machine]
-      @method = options[:method] || :post
-
-      # Generate an initial call SID if we don't have one
-      if (options[:call_sid].nil?)
-        @sid = UUIDTools::UUID.random_create.to_s
-      else
-        @sid = options[:call_sid]
-      end
-
-      if options[:is_machine].nil?
-        options[:is_machine] = @is_machine
-      end
-
-      if options[:method].nil?
-        options[:method] = @method
-      end
+      @method = options[:method]
+      @sid = options[:call_sid] ||= UUIDTools::UUID.random_create.to_s
 
       # We are the root call
       self.root_call = self
